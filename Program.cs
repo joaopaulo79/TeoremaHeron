d92de8db06c94596978239e412297db53bc0314c
João Paulo <joaopdejesus79@gmail.com>
2021-09-25 11:19:44 -0300

    Programa Finalizado!

--- Program.cs
+++ Program.cs
@@ -6,45 +6,36 @@ namespace TeoremaHeron
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            double l1, l2, l3, p, a;
+
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("==== Teorema de Heron =====");
+            Console.WriteLine("\n==== Teorema de Heron =====\n");
             Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine("Digite a medida dos 3 lados\nde seu triângulo, para que\neu possa calcular.");
-            Console.WriteLine();
+            
+            Console.WriteLine("Digite a medida dos 3 lados\nde seu triângulo, para que\neu possa calcular a área.\n");
+            
+            Console.Write("Lado 1...: ");
+            l1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Lado 2...: ");
+            l2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Lado 3...: ");
+            l3 = Convert.ToDouble(Console.ReadLine());
+
+            p = (l1 + l2 + l3) / 2;
+
+            a = Math.Sqrt(p * (p - l1) * (p - l2) * (p - l3));
+
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.Write("Medida do primeiro lado..: ");
+            Console.WriteLine("\n== Resultado ==\n");
             Console.ResetColor();
-            string lado1 = Console.ReadLine();
-            double L1 = Convert.ToDouble(lado1);
+
+            Console.WriteLine($"Semiperímetro..= {p}");
+            
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.Write("Medida do segundo lado...: ");
+            Console.WriteLine($"Área...........= {a}\n");
             Console.ResetColor();
-            string lado2 = Console.ReadLine();
-            double L2 = Convert.ToDouble(lado2);
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.Write("Medida do terceiro lado..: ");
-            Console.ResetColor();
-            string lado3 = Console.ReadLine();
-            double L3 = Convert.ToDouble(lado3);
-            double SP = (L1 + L2 + L3) / 2;
-            double H = Math.Sqrt(SP * (SP - L1) * (SP - L2) * (SP - L3));
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("== Resultado ==");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine($"Lado 1........= {L1}");
-            Console.WriteLine($"Lado 2........= {L2}");
-            Console.WriteLine($"Lado 3........= {L3}");
-            Console.WriteLine($"Semipeímetro..= {SP}");
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.Write($"Área..........= {H:N2}");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
         }
     }
 }
